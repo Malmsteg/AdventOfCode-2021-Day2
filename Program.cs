3f7dffdd7ebabea839ce318998b620540e99b679
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-12-02 06:11:36 +0100

    Finished

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace AdventOfCode_2021_Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string file = Path.Combine(currentDirectory, @"..\..\..\input.txt");
+            string path = Path.GetFullPath(file);
+            string[] text = File.ReadAllText(path).Split("\n");
+
+            int depth = 0;
+            int distance = 0;
+            int aim = 0;
+
+            for(int i = 0; i < text.Length; i++){
+                string[] input = text[i].Split(' ');
+                int amount = Convert.ToInt32(input[1]);
+                switch(input[0]){
+                    case "forward":
+                    distance+=amount;
+                    depth+= aim*amount;
+                    break;
+                    case "down":
+                    // depth+=amount;
+                    aim+=amount;
+                    break;
+                    case "up":
+                    // depth-=amount;
+                    aim-=amount;
+                    break;
+                    default:
+                    break;
+                }
+            }
+            Console.WriteLine(depth*distance);
+
+        }
+    }
+}
